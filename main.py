10e49a5376a17648ff02f20a12805cea4be44b83
Christian Yurianja <chris37fx@gmail.com>
2021-10-05 09:51:51 +0700

    Correct what moms want

--- main.py
+++ main.py
@@ -7,8 +7,30 @@ Semua sintaksis dasar bahasa pemrograman terdiri dari:
 
 
 # Sekuensial
+# print('Ibu berkata, "Pergi ke toko"')
+# print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+# print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+# print("Maka Budi berangkat ke toko")
+# print("Dan mulai berbelanja")
+
+# Percabangan
 print('Ibu berkata, "Pergi ke toko"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
-print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
-print("Maka Budi berangkat ke toko")
-print("Dan mulai berbelanja")
+print('Ibu menjawab, "Beli 1 botol susu, dan jika ada telor beli 6 telor"\n')
+
+jumlah_botol_susu = 10
+jumlah_telur = 6
+print(f"Jumlah botol susu {jumlah_telur} botol")
+print(f"Jumlah telur {jumlah_telur} butir\n")
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Anak Pulang ke rumah")
+if jumlah_telur > 0:
+    print("Budi membeli 6 butir telur\n")
+else:
+    pass
+print("Budi pulang ke rumah")
+print("Menyerahkan hasil belanjanya kepada Ibu")
